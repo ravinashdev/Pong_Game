577b135a082ce488100189bd9d3e2f7fdc389055
Ryan Ramdehol <ravinashwork@gmail.com>
2026-03-19 20:58:30 -0400

    add another paddle or second player

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 from turtle import Screen
-
+import time
 from paddle import Paddle
 
 # Initialize Screen Object
 screen = Screen()
-screen.setup(width=1680, height=1050)
+screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong Game Atari")
 screen.tracer(0)
 
+# Initialize First Paddle
+paddle_1 = Paddle(350,0)
+# Initialize Second Paddle
+paddle_2 = Paddle(-350,0)
+
+# # Listen for key events to invoke methods to change direction
+screen.listen()
+screen.onkey(paddle_1.up, "Up")
+screen.onkey(paddle_1.down, "Down")
+screen.onkey(paddle_2.up, "w")
+screen.onkey(paddle_2.down, "s")
 
 
-
-
-
-
-
-
-
-
-
-
-
+# Initialize Game
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.05)
 # Screen exit on click
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,4 +1,17 @@
 from turtle import Turtle
+
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, xcor, ycor):
         super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.setposition(xcor,ycor)
+    def up(self):
+        new_y_coordinate = self.ycor() + 20
+        self.setposition(self.xcor(), new_y_coordinate)
+    def down(self):
+        new_y_coordinate = self.ycor() - 20
+        self.setposition(self.xcor(), new_y_coordinate)
+
