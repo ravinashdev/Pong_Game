77b22f1760b8260313e01b73eecb1f12865a47d5
Ryan Ramdehol <ravinashwork@gmail.com>
2026-03-22 23:02:15 -0400

    got paddle logic working must tweak coordinates more to get it to be smoother

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ screen.bgcolor("black")
 screen.title("Pong Game Atari")
 screen.tracer(0)
 
-# Initialize First Paddle
+# Initialize First Paddle Object
 paddle_1 = Paddle(350,0)
-# Initialize Second Paddle
+# Initialize Second Paddle Object
 paddle_2 = Paddle(-350,0)
-# Initialize Ball
+# Initialize Ball Object
 pong_ball = PongBall()
 # # Listen for key events to invoke methods to change direction
 screen.listen()
@@ -27,7 +27,15 @@ screen.onkey(paddle_2.down, "s")
 # Initialize Game
 game_on = True
 while game_on:
+    pong_ball.move()
     screen.update()
-    time.sleep(0.05)
+    time.sleep(0.1)
+    # Wall Collision
+    if abs(pong_ball.ycor()) >= 280:
+        pong_ball.bounce()
+    # Paddle Collision
+    elif abs(pong_ball.xcor()) >= 330 and (pong_ball.distance(paddle_1) < 50 or pong_ball.distance(paddle_2) < 50):
+        pong_ball.paddle_hit()
+
 # Screen exit on click
 screen.exitonclick()
--- pong_ball.py
+++ pong_ball.py
@@ -1,9 +1,27 @@
 from turtle import Turtle
+BALL_POSITIONS = [(0,0),(20,20),(40,40),(60,60),(80,80),(100,100),(120,120)]
+
 class PongBall(Turtle):
     def __init__(self):
         super().__init__()
         self.shape("circle")
         self.color("blue")
         self.penup()
-        self.shapesize(stretch_wid=2, stretch_len=2)
         self.setposition(0, 0)
+        self.x_increment = 20
+        self.y_increment = 20
+        self.x_coordinate = self.xcor()
+        self.y_coordinate = self.ycor()
+    def move(self):
+        new_y_coordinate = self.ycor() + self.y_increment
+        new_x_coordinate = self.xcor() + self.x_increment
+        self.setposition(new_x_coordinate, new_y_coordinate)
+    def bounce(self):
+        self.y_increment*=-1
+    def paddle_hit(self):
+        self.x_increment*=-1
+
+
+
+
+
