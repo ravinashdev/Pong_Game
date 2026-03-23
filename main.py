ef333bad9935c536a720c09a1af3314179582937
Ryan Ramdehol <ravinashwork@gmail.com>
2026-03-23 01:45:02 -0400

    logic to make net based on height of screen and game_over condition to end it when the first player reaches 11

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import time
 from paddle import Paddle
 from pong_ball import PongBall
 from score_board import ScoreBoard
+from net import Net
 
 # Initialize Screen Object
 screen = Screen()
-screen.setup(width=800, height=600)
+screen_width = 800
+screen_height = 600
+screen.setup(screen_width, screen_height)
 screen.bgcolor("black")
 screen.title("Pong Game Atari")
 screen.tracer(0)
@@ -17,7 +20,10 @@ paddle_1 = Paddle(350,0)
 paddle_2 = Paddle(-350,0)
 # Initialize Ball Object
 pong_ball = PongBall()
-# Initialize ScoreBoard for each player
+# Initialize Net line
+net = Net(screen_height)
+net.draw_line()
+# Initialize ScoreBoard for each player with a starting score of 0
 paddle_1_score = ScoreBoard(x_coordinate= 175,y_coordinate= 200)
 paddle_2_score = ScoreBoard(x_coordinate= -175, y_coordinate=200)
 paddle_1_score.write_score()
@@ -29,7 +35,6 @@ screen.onkey(paddle_1.up, "Up")
 screen.onkey(paddle_1.down, "Down")
 screen.onkey(paddle_2.up, "w")
 screen.onkey(paddle_2.down, "s")
-
 
 # Initialize Game
 game_on = True
@@ -44,15 +49,21 @@ while game_on:
     elif abs(pong_ball.xcor()) >= 320 and (pong_ball.distance(paddle_1) < 50 or pong_ball.distance(paddle_2) < 50):
         pong_ball.x_inverse()
         # pong_ball.ball_speed_increase()
-    # PongBall passes any of the paddles and reset
+    # PongBall passes any of the paddles one player scores and the ball resets
     elif abs(pong_ball.xcor()) >= 350:
         # Logic to increase scoreboard respectively as it passes the paddle of one player
         if pong_ball.xcor() > 350:
             paddle_2_score.increase_score()
             paddle_2_score.write_score()
-        elif pong_ball.xcor() < -340:
+        elif pong_ball.xcor() < -350:
             paddle_1_score.increase_score()
             paddle_1_score.write_score()
         pong_ball.reset_position()
+    # Game over condition for when the first player reaches 11
+    elif (paddle_1_score.score or paddle_2_score.score) == 11:
+        game_over = ScoreBoard(x_coordinate= 0,y_coordinate= 0)
+        net.clear()
+        game_over.game_over()
+        game_on = False
 # Screen exit on click
 screen.exitonclick()
--- net.py
+++ net.py
@@ -1,4 +1,18 @@
 from turtle import Turtle
 class Net(Turtle):
-    def __init__(self):
+    def __init__(self, screen_height):
         super().__init__()
+        self.color("white")
+        self.penup()
+        self.setposition(0,-300)
+        self.setheading(90)
+        self.hideturtle()
+        self.pensize(10)
+        self.screen_height = screen_height
+    def draw_line(self):
+        for i in range(0,self.screen_height,100):
+            self.pendown()
+            self.forward(50)
+            self.penup()
+            self.forward(50)
+
